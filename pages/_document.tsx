8e26528e6c6305518cb3c090609678c0c4ce05fe
lamenking <dnwjddus1506@hanmail.net>
2022-04-22 16:40:57 +0900

    🌱 Set up to use styled-components

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheets } from '@mui/styles';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
 	render() {
@@ -17,20 +18,28 @@ export default class MyDocument extends Document {
 }
 
 MyDocument.getInitialProps = async (ctx) => {
+	const sheet = new ServerStyleSheet();
 	const materialSheets = new ServerStyleSheets();
 	const originalRenderPage = ctx.renderPage;
 
-	ctx.renderPage = () =>
-		originalRenderPage({
-			enhanceApp: (App) => (props) =>
-				materialSheets.collect(<App {...props} />),
-		});
+	try {
+		ctx.renderPage = () =>
+			originalRenderPage({
+				enhanceApp: (App) => (props) =>
+					sheet.collectStyles(materialSheets.collect(<App {...props} />)),
+			});
 
-	const initialProps = await Document.getInitialProps(ctx);
-	return {
-		...initialProps,
-		styles: [
-			<React.Fragment key={'key'}>{initialProps.styles}</React.Fragment>,
-		],
-	};
+		const initialProps = await Document.getInitialProps(ctx);
+		return {
+			...initialProps,
+			styles: [
+				<React.Fragment key={'key'}>
+					{initialProps.styles}
+					{sheet.getStyleElement()}
+				</React.Fragment>,
+			],
+		};
+	} finally {
+		sheet.seal();
+	}
 };
